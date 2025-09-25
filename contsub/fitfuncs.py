8afe1b173d1c057ced804f2c18b78b9fefdccafd
sphemakh <sphemakh@gmail.com>
2025-09-25 09:52:38 +0200

    add median-filter and dct baseline fits

--- contsub/fitfuncs.py
+++ contsub/fitfuncs.py
@@ -1,48 +1,77 @@
 from scipy.interpolate import splev, splrep
 import sys
 from scabha import init_logger
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 from . import BIN
+from .exceptions import BadFitError
 import numpy as np
+from scipy import fftpack
 
 log = init_logger(BIN.im_plane)
-
 
 class FitFunc:
     """
     abstract class for writing fitting functions
     """
-    def __init__(self, order, velwidth, cont_tol):
+    def __init__(self, freqs, velwidth:float, fit_tol:float=0):
         """
 
         Args:
             order (_type_): _description_
             velwidth (_type_): _description_
         """
-        self.order = order
         self.velwidth = velwidth
-        self.preped = False
-        self.cont_tol = cont_tol
-    
-    def prepare(self, x):
-        nchan = len(x)
-        msort = np.argpartition(x, -2)
+        self.fit_tol = fit_tol
+        self.freqs = freqs
+        self.nchan = freqs.size
+
+    def prepare(self):
+        freqs = self.freqs
+        msort = np.argpartition(freqs, -2)
         m1l, m2l = msort[-2:]
         m1h, m2h = msort[:2]
         if np.abs(m1l - m2l) == 1 and np.abs(m1h - m2h) == 1:
-            dvl = np.abs(x[m1l]-x[m2l])/np.mean([x[m1l],x[m2l]])*3e5
-            dvh = np.abs(x[m1h]-x[m2h])/np.mean([x[m1h],x[m2h]])*3e5
-            self.dv = (dvl+dvh)/2
-            self.imax = int(nchan / (self.velwidth//self.dv))+1
+            dvl = np.abs(freqs[m1l]-freqs[m2l])/np.mean([freqs[m1l],freqs[m2l]])*3e5
+            dvh = np.abs(freqs[m1h]-freqs[m2h])/np.mean([freqs[m1h],freqs[m2h]])*2.998e5
+            dv = (dvl+dvh)/2
+            self.imax = int(self.velwidth/dv)
+            if self.imax %2 == 0:
+                self.imax += 1
+            log.info(f"nchan = {self.nchan}, dv = {dv}, {self.velwidth}km/s in chans: {self.imax}")
         else:
-            log.error('The frequency values are not changing monotonically, aborting')
+            log.error('probably x values are not changing monotonically, aborting')
             sys.exit(1)
-            
-        
-        log.info(f"nchan = {nchan}, dv = {self.dv}, {self.velwidth}km/s in chans:"
-                f" {self.velwidth//self.dv}, max order spline = {self.imax}")
         self.preped = True
 
+    def invalid_point_count(self, data:np.ndarray, mask:np.ndarray):
+        """ Calculates the number of invalid data points in a spectrum.
+
+        Args:
+            data (np.ndarray): 1D spectrum
+            mask (np.ndarray): Binary mask
+        """
+        data_count = np.isnan(data).sum()
+        mask_count = mask.sum()
+        
+        return data_count + mask_count
+    
+    def is_fit_possible(self, data: np.ndarray, mask: np.ndarray, raise_exception=True):
+        """_summary_
+
+        Args:
+            data (np.ndarray): _description_
+            mask (np.ndarray): _description_
+        """
+        invalid = self.invalid_point_count(data, mask)
+        nchan = data.size
+        valid_fraction = (1 - invalid / nchan) * 100
+        if valid_fraction < self.fit_tol:
+            if raise_exception:
+                raise BadFitError(f"Fraction of valid data points {valid_fraction:.2f} is less than the required tolerance (--cont-fit-tol {self.fit_tol})")
+            else:
+                return False
+        else:
+            return True
     
     @abstractmethod
     def fit(self, x, data, mask, weight):
@@ -52,13 +81,19 @@ class FitBSpline(FitFunc):
     """
     BSpline fitting function based on `splev`, `splrep` in `scipy.interpolate` 
     """
-    def __init__(self, order, velWidth, randomState=None, seq=None):
+    def __init__(self, freqs, order, velwidth, fit_tol=0, randomState=None, seq=None):
         """
         needs to know the order of the spline and the number of knots
         """
         self.order = order
-        self.velwidth = velWidth
-        self.preped = False  
+        self.velwidth = velwidth
+        self.fit_tol = fit_tol
+        self.freqs = freqs
+        self.nchan = freqs.size
+        self.prepare() 
+        self.max_spline_order = int(self.nchan / self.imax) + 1
+        log.info(f"max spline order: {self.max_spline_order}")
+
         if randomState and seq:
             rs = np.random.SeedSequence(entropy = randomState, spawn_key = (seq,))
         else:
@@ -66,7 +101,7 @@ class FitBSpline(FitFunc):
         self.rng = np.random.default_rng(rs)
         
         
-    def fit(self, x, data, weights):
+    def fit(self, data: np.ndarray, mask: np.ndarray, weights: np.ndarray):
         """
         returns the spline fit and the residuals from the fit
         
@@ -76,61 +111,148 @@ class FitBSpline(FitFunc):
             To mask values, set the corresponding weight to zero.
         """
 
-        if not self.preped:
-            self.prepare(x)
+        self.is_fit_possible(data, mask, raise_exception=True)
+        nvalid = self.nchan - self.invalid_point_count(data, mask)
+        
+        if nvalid < (self.order + 1):
+            raise BadFitError("Not enough valid points for spline fit, returning original data.")
             
         # Mask invalid or zero-weight points
-        mask = (weights > 0) & np.isfinite(x) & np.isfinite(data) & np.isfinite(weights)
-        x_masked = x[mask]
-        data_masked = data[mask]
-        weights_masked = weights[mask]
+        mask[np.where(np.isnan(data))] = True
+        x_masked = self.freqs[~mask] 
+        data_masked = data[~mask]
     
-    
-        if len(x_masked) < (self.order + 1):
-            #TODO(Sphe) maybe return raise an exception, and let the caller of this function decide what to do.
-            log.debug("Not enough valid points for spline fit, returning original data.")
-            return np.copy(data)  # fallback: just return input
-    
-        nchan = len(x_masked)
-        knotind = np.linspace(0, nchan, self.imax, dtype=int)[1:-1]
-        chwid = max(1, (nchan // self.imax) // 8)
+        knotind = np.linspace(0, x_masked.size, self.max_spline_order, dtype=int)[1:-1]
+        chwid = max(1, (self.nchan // self.max_spline_order) // 8)
         knots_idx = self.rng.integers(-chwid, chwid, size=knotind.shape) + knotind
-        knots_idx = np.clip(knots_idx, 1, nchan - 20)  # avoid edges
-        knot_positions = np.unique(x_masked[knots_idx])
-    
-        splCfs = splrep(x_masked, data_masked, task=-1,
-                        w=weights_masked, t = knot_positions, k=self.order)
-        spl = splev(x, splCfs) 
-        return spl
+        knots_idx = np.unique(np.clip(knots_idx, 1, x_masked.size-1)) # avoid edges
+        
+        knot_positions = x_masked[knots_idx]
+        
+        if isinstance(weights, np.ndarray):
+            splCfs = splrep(x_masked, data_masked, task=-1,
+                            w=weights[~mask], t = knot_positions, k=self.order)
+        else:
+            splCfs = splrep(x_masked, data_masked, task=-1,
+                            t=knot_positions, k=self.order)
+
+        return  splev(self.freqs, splCfs)
+
 
 class FitMedFilter(FitFunc):
     """
     Median filtering class for continuum subtraction 
     """
-    def __init__(self, velWidth):
+    def __init__(self, freqs, velwidth, fit_tol=0):
         """
         needs to know the order of the spline and the number of knots
         """
-        self._velwid = velWidth
+        self.velwidth = velwidth
+        self.fit_tol = fit_tol
+        self.freqs = freqs
+        self.nchan = freqs.size
+        self.prepare()
+    
+            
+    def fit(self, data: np.ndarray, mask: np.ndarray, weights: np.ndarray):
+        """
+        returns the median filtered data as line emission
         
-    def prepare(self, x):
-        msort = np.argpartition(x, -2)
-        m1l, m2l = msort[-2:]
-        m1h, m2h = msort[:2]
-        if np.abs(m1l - m2l) == 1 and np.abs(m1h - m2h) == 1:
-            dvl = np.abs(x[m1l]-x[m2l])/np.mean([x[m1l],x[m2l]])*3e5
-            dvh = np.abs(x[m1h]-x[m2h])/np.mean([x[m1h],x[m2h]])*3e5
-            dv = (dvl+dvh)/2
-            self._imax = int(self._velwid//dv)
-            if self._imax %2 == 0:
-                self._imax += 1
-            log.info('len(x) = {}, dv = {}, {}km/s in chans: {}'.format(len(x), dv, self._velwid, self._velwid//dv))
-        else:
-            log.debug('probably x values are not changing monotonically, aborting')
+        data (np.ndarray) : values to be fit
+        mask (np.ndarray) : a mask (not implemented really)
+        weight (np.ndarray) : weights
+        """
+        self.is_fit_possible(data, mask, raise_exception=True)
+        
+        if isinstance(mask, np.ndarray):
+            data[mask] = np.nan
+        
+        padded_data = np.pad(data, self.imax//2, mode="linear_ramp")
+        filtered = np.nanmedian(np.lib.stride_tricks.sliding_window_view(padded_data, self.imax), axis = 1)
+        
+        return filtered
+
+class FitPolynomial(FitFunc):
+    """
+    Polynomial fitting function using numpy.polyfit
+    """
+    def __init__(self, freqs, order, fit_tol=0):
+        """
+        order (int): Order/degree of the polynomial
+        """
+        self.order = order
+        self.fit_tol = fit_tol
+        self.freqs = freqs
+        self.nchan = freqs.size
+        self.prepare()
+        
+    def prepare(self):
+        """
+        Prepare for polynomial fitting 
+        """
+        
+        log.info(f"Polynomial fitting: nchan = {self.nchan}, order = {self.order}")
+        self.preped = True 
+    
+    def fit(self, data: np.ndarray, mask:np.ndarray, weights: np.ndarray):
+        """_summary_
+
+        Args:
+            x (np.ndarray): _description_
+            data (np.ndarray): _description_
+            weights (np.ndarray): _description_
+            mask (np.ndarray): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        
+        self.is_fit_possible(data, mask, raise_exception=True)
+        
+        
+        x_masked = self.freqs[~mask]
+        data_masked = data[~mask]
+            
+        try:
+            if isinstance(weights, np.ndarray):
+                coeffs = np.polyfit(x_masked, data_masked, self.order, w=weights[~mask])
+            else:
+                coeffs = np.polyfit(x_masked, data_masked, self.order)
+            return np.polyval(coeffs, self.freqs)
+        
+        except Exception as e:
+            log.error(f"Polynomial fitting failed: {e}")
             sys.exit(1)
             
-    
-    def fit(self, x, data, mask, weight):
+class FitDCT(FitFunc):
+    """
+    Median filtering class for continuum subtraction 
+    """
+    def __init__(self, freqs, velwidth, ncoef, dct_type=1, fit_tol=0):
+        """
+        needs to know the order of the spline and the number of knots
+        """
+        self.velwidth = velwidth
+        self.fit_tol = fit_tol
+        self.preped = False
+        self.dct_type = dct_type
+        self.ncoef = ncoef
+        self.freqs = freqs
+        self.nchan = freqs.size
+        
+        fnorm_dict = {
+        1: 1 / np.sqrt( 2 * self.nchan),
+        2: np.sqrt(2 / (self.nchan - 1)) / 2,
+        3: 1 / np.sqrt( 2 * self.nchan),
+        4: 1 / np.sqrt( 2 * self.nchan),
+        }
+        
+        self.fnorm = fnorm_dict[dct_type]
+        print(self.fnorm)
+        
+        self.prepare()
+            
+    def fit(self, data: np.ndarray, mask: np.ndarray, weights: np.ndarray):
         """
         returns the median filtered data as line emission
         
@@ -139,67 +261,16 @@ class FitMedFilter(FitFunc):
         mask : a mask (not implemented really)
         weight : weights
         """
-        if not (mask is None):
-            data[np.logical_not(mask)] = np.nan
+        baseline = FitMedFilter(self.freqs, self.velwidth)
         
-        if self._imax%2 == 0:
-            window = self._imax + 1
-        else:
-            window = self._imax
+        baseline_median = baseline.fit(data, mask=mask, weights=None)
         
-        padded_data = np.pad(data, window//2, mode="linear_ramp")
-        filtered = np.nanmedian(np.lib.stride_tricks.sliding_window_view(padded_data, window), axis = 1)
         
-        return filtered
+        dct_data = fftpack.dct(baseline_median, type=self.dct_type)
+        sort_idx = np.argsort(np.absolute(dct_data))[:-self.ncoef]
+        dct_data[sort_idx] = 0
+        
+        dct_fit = fftpack.idct(dct_data, type=self.dct_type) * self.fnorm**2
+        
+        return dct_fit
 
-class FitPolynomial(FitFunc):
-    """
-    Polynomial fitting function using numpy.polyfit
-    """
-    def __init__(self, order, cont_tol = 0):
-        """
-        order (int): Order/degree of the polynomial
-        """
-        self.order = order
-        self.preped = False
-        self.cont_tol = cont_tol
-        
-    def prepare(self, x):
-        """
-        Prepare for polynomial fitting 
-        """
-        nchan = len(x)
-        log.info(f"Polynomial fitting: nchan = {nchan}, order = {self.order}")
-        self.preped = True
-    
-    def fit(self, x, data, weights=None):
-        if not self.preped:
-            self.prepare(x)
-
-        if weights is not None:
-            mask = (weights > 0) & np.isfinite(x) & np.isfinite(data) & np.isfinite(weights)
-            x_masked = x[mask]
-            data_masked = data[mask]
-            weights_masked = weights[mask]
-        
-        else:
-            mask = np.isfinite(x) & np.isfinite(data)
-            x_masked = x[mask]
-            data_masked = data[mask]
-            weights_masked = None
-            
-        if (len(x_masked)/len(x))*100 <= self.cont_tol:
-            #TODO(Sphe) Not sure if this is the right thing to do
-            return np.zeros_like(data)
-
-        try:
-            if weights_masked is not None:
-                coeffs = np.polyfit(x_masked, data_masked, self.order, w=weights_masked)
-            else:
-                coeffs = np.polyfit(x_masked, data_masked, self.order)
-            return np.polyval(coeffs, x)
-        except Exception as e:
-            log.debug(f"Polynomial fitting failed: {e}")
-            
-        # TODO(Sphe) Rather raise an exception and let caller of function decide what to do.
-        return data
--- contsub/parser/imcontsub.py
+++ contsub/parser/imcontsub.py
@@ -8,7 +8,12 @@ import os
 from contsub import BIN
 from scabha import init_logger
 from contsub.image_plane import ContSub
-from contsub.fitfuncs import FitBSpline
+from contsub.fitfuncs import (
+    FitBSpline,
+    FitPolynomial,
+    FitMedFilter,
+    FitDCT,
+)
 import astropy.io.fits as fitsio
 from contsub.utils import zds_from_fits, get_automask, subtract_fits
 import dask.array as da
@@ -51,16 +56,13 @@ def runit(**kwargs):
     if opts.overwrite is False and (outcont.EXISTS or outline.EXISTS):
         raise RuntimeError("At least one output file exists, but --no-overwrite has been set. Unset it to proceed.")
 
-    if opts.segments is None:
-        raise RuntimeError("Required option 'segments' is not set. Please run 'imcontsub --help' for more information.")
-        
-    if len(opts.order) != len(opts.segments):
-        raise ValueError("If setting multiple --order and --segments, they must be of equal length. "
-                    f"Got {len(opts.order)} orders and {len(opts.segments)} segments.")
-    niter = len(opts.order)
-
-
+    if opts.fit_model in "spline polynomial dct".split() and not getattr(opts, "order", False):
+        raise RuntimeError("The parameter 'order' is required for fit-model={opts.fit_model}.")
     
+    if opts.fit_model in "spline median-filter dct".split() and not getattr(opts, "vel_width", False):
+        raise RuntimeError("The parameter 'vel-width' is required for fit-model={opts.fit_model}.")
+    
+    velwidth = opts.vel_width or opts.segments
     chunks = dict(ra = opts.ra_chunks or 64, dec=None, spectral=None)
     
     rest_freq = opts.rest_freq
@@ -83,69 +85,57 @@ def runit(**kwargs):
     
     
     nomask = True
-    automask = False 
     if getattr(opts, "mask_image", None):
         mask = zds_from_fits(opts.mask_image, chunks=chunks, rest_freq=rest_freq).DATA
         nomask = False
     
-    if getattr(opts, "sigma_clip", None):
-        automask = True
-        sigma_clip = list(opts.sigma_clip)
-    else:
-        sigma_clip = []
-    
-    if len(sigma_clip) < niter and automask:
-        log.warning(f"Only {len(sigma_clip)} sigma-clips provided, but {niter} iterations requested."
-                    " Using last value for unspecified iterations.")
-        sigma_clip.extend([sigma_clip[-1]] * (niter - len(sigma_clip)))
-    
     get_mask = da.gufunc(
         get_automask,
-        signature=f"(spectral),({dims_string}),(),(),() -> ({dims_string})",
+        signature=f"(spectral),({dims_string}) -> ({dims_string})",
         meta=(np.ndarray((), cube.dtype),),
         allow_rechunk=True,
     )
 
-    signature = f"(spectral),({dims_string}),({dims_string}) -> ({dims_string})"
+    signature = f"({dims_string}),({dims_string}) -> ({dims_string})"
     meta = (np.ndarray((), cube.dtype),)
-    xspec = zds.FREQS.data
+    xspec = zds.FREQS.data.compute()
     
     dask.config.set(scheduler='threads', num_workers = opts.nworkers)
     dblocks = cube.data.blocks
-    for iter_i in range(niter):
-        log.info(f"Loading delayed compute for iteration {iter_i+1}/{niter} of continuum modelling.")
-        futures = []
-        fitfunc = FitBSpline(opts.order[iter_i], opts.segments[iter_i])
-        for biter,dblock in enumerate(dblocks):
-            if (nomask and automask) or (iter_i > 0 and automask):
-                mask_future = get_mask(xspec,
-                                    dblock,
-                                    sigma_clip[iter_i], 
-                                    opts.order[iter_i],
-                                    opts.segments[iter_i],
-                )
-            elif nomask is False:
-                mask_future = mask.data.blocks[biter] == False
-            else:
-                mask_future = da.ones_like(dblock, dtype=bool)
-            
-            contfit = ContSub(fitfunc, nomask=False,
-                            fit_tol=opts.cont_fit_tol)
-            
-            getfit = da.gufunc(
-                contfit.fitContinuum,
-                signature=signature,
-                meta=meta,
-                allow_rechunk=True,
-            )
-            
-            futures.append(getfit(
-                xspec,
-                dblock,
-                mask_future,
-            ))
-            
-        dblocks = list(futures)
+    futures = []
+    
+    if opts.fit_model == "spline":
+        fitfunc = FitBSpline(xspec, opts.order, velwidth, fit_tol=opts.cont_fit_tol)
+    elif opts.fit_model == "polynomial":
+        fitfunc = FitPolynomial(xspec, opts.order, fit_tol=opts.cont_fit_tol)
+    elif opts.fit_model == "median-filter":
+        fitfunc = FitMedFilter(xspec, velwidth, fit_tol=opts.cont_fit_tol)
+    elif opts.fit_model == "dct":
+        fitfunc = FitDCT(xspec, velwidth, opts.order, fit_tol=opts.cont_fit_tol)
+        
+    for biter,dblock in enumerate(dblocks):
+        if opts.sigma_clip:
+            mask_future = get_mask(dblock, fitfunc, sigma_clip=opts.sigma_clip)
+        elif nomask is False:
+            mask_future = mask.data.blocks[biter]
+        else:
+            mask_future = da.zeros_like(dblock, dtype=bool)
+        
+        contfit = ContSub(fitfunc, nomask=False)
+        
+        getfit = da.gufunc(
+            contfit.fitContinuum,
+            signature=signature,
+            meta=meta,
+            allow_rechunk=True,
+        )
+        
+        futures.append(getfit(
+            dblock,
+            mask_future,
+        ))
+        
+    dblocks = list(futures)
     
     continuum = da.concatenate(futures).transpose((2,1,0))
     if has_stokes:
@@ -154,12 +144,12 @@ def runit(**kwargs):
     header = zds.attrs["header"]
     out_ds_cont = fitsio.PrimaryHDU(continuum, header=header)
     
-    out_ds_cont.writeto(outcont, overwrite=opts.overwrite)
+    out_ds_cont.writeto(outcont.PATH, overwrite=opts.overwrite)
     log.info(f"Continuum model cube written to: {outcont}")
     
-    out_ds_line = subtract_fits(infits, outcont, chunks={0: opts.ra_chunks, 1:None, 2:None})
+    out_ds_line = subtract_fits(infits.PATH, outcont.PATH, chunks={0: opts.ra_chunks, 1:None, 2:None})
     log.info(f"Writing residual data (line cube) to: {outline}")
-    out_ds_line.writeto(outline, overwrite=opts.overwrite)
+    out_ds_line.writeto(outline.PATH, overwrite=opts.overwrite)
 
     # DONE
     dtime = time.time() - start_time
